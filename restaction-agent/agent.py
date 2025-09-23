487f90c526a6e915a33b384b5c42127e0cae9c70
EdmondDantes21 <redivreto@gmail.com>
2025-09-23 09:57:13 +0200

    added restaction agent

---
+++ restaction-agent/agent.py
@@ -0,0 +1,15 @@
+from google.adk.agents import Agent
+
+# --- Models ---
+GEMINI_2_5_PRO = "gemini-2.5-pro"
+
+# --- Prompts ---
+PROMPT = open("prompts/restaction-agent.md").read() 
+ 
+# --- Root Agent ---    
+root_agent = Agent(
+    name="krateo_autopilot",
+    model=GEMINI_2_5_PRO,
+    description="RESTACTION Agent for Krateo Autopilot.",
+    instruction=PROMPT,
+)
