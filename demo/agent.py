830faf4240f77f59c3d3362e07cd15e7a211ecbe
EdmondDantes21 <redivreto@gmail.com>
2025-10-14 09:22:24 +0200

    improve agent description for delegation

--- demo/agent.py
+++ demo/agent.py
@@ -2,7 +2,7 @@ from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnecti
 from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
 from google.adk.tools import agent_tool
 from google.adk.agents import Agent
-from . import tools, git_tools
+from . import tools, git_tools, tools_portal_agent
 import os
 
 # --- Models ---
@@ -13,8 +13,8 @@ GEMINI_2_5_PRO = "gemini-2.5-pro"
 DOCUMENTATION_AGENT_PROMPT = open("prompts/documentation_agent.md").read()
 ROOT_AGENT_PROMPT = open("prompts/krateo_autopilot_2.md").read()
 AUTHENTICATION_AGENT_PROMPT = open("prompts/authentication_agent.md").read()
-COMPOSITION_AGENT_PROMPT = open("prompts/agent-tool/composition-agent-5.md").read()
-PORTAL_AGENT_PROMPT = open("prompts/portal-agent-granular/portal-agent-4.md").read() 
+COMPOSITION_AGENT_PROMPT = open("prompts/agent-tool/composition-agent-6.md").read()
+PORTAL_AGENT_PROMPT = open("portal-agent/portal-agent.md").read() 
 RESTACTION_AGENT_PROMPT = open("prompts/restaction-agent.md").read() 
 GITHUB_AGENT_PROMPT = open("prompts/agent-tool/github-agent.md").read()
 
@@ -25,7 +25,9 @@ try:
         model=GEMINI_2_5_FLASH,
         name='github_agent',
         instruction=GITHUB_AGENT_PROMPT,
-        description="Creates repositories and manages files in GitHub.", # Crucial for delegation
+        description="This agent is specialized for performing Git and GitHub operations." # Crucial for delegation
+        "It directly interacts with the GitHub API and the user's local filesystem to manage repositories." 
+        "Delegate a user's request to this agent if the intent involves creating or modifying repositories, branches, files, or pull requests on GitHub.",
         tools=[
             MCPToolset(
                 connection_params=StreamableHTTPConnectionParams(
@@ -45,21 +47,19 @@ try:
 except Exception as e:    
     print(f"❌ Could not create Github agent. Check API Key ({github_agent.model}). Error: {e}")
     
-github_agent_tool = agent_tool.AgentTool(agent=github_agent) # Wrap the agent
-
 # --- Restaction Agent ---
-restaction_agent = None
-try: 
-    restaction_agent = Agent(
-        name="restaction_agent",
-        model=GEMINI_2_5_PRO,
-        description="RESTACTION Agent for Krateo Autopilot.", # crucial for delegation
-        instruction=RESTACTION_AGENT_PROMPT,
-    )
-    print(f"✅ Agent '{restaction_agent.name}' created using model '{restaction_agent.model}'.")
-except Exception as e:
-    print(f"❌ Could not create '{restaction_agent.name}' agent. Check API Key ({restaction_agent.model}). Error: {e}")
-restaction_agent_tool = agent_tool.AgentTool(agent=restaction_agent) # Wrap the agent
+# restaction_agent = None
+# try: 
+#     restaction_agent = Agent(
+#         name="restaction_agent",
+#         model=GEMINI_2_5_PRO,
+#         description="RESTACTION Agent for Krateo Autopilot.", # crucial for delegation
+#         instruction=RESTACTION_AGENT_PROMPT,
+#     )
+#     print(f"✅ Agent '{restaction_agent.name}' created using model '{restaction_agent.model}'.")
+# except Exception as e:
+#     print(f"❌ Could not create '{restaction_agent.name}' agent. Check API Key ({restaction_agent.model}). Error: {e}")
+# restaction_agent_tool = agent_tool.AgentTool(agent=restaction_agent) # Wrap the agent
 
 # --- Portal Agent ---
 portal_agent = None
@@ -67,15 +67,13 @@ try:
     portal_agent = Agent(
         name="portal_agent",
         model=GEMINI_2_5_PRO,
-        description="Portal Agent for Krateo Autopilot.",
+        description="Creates and manages portal sections (Krateo's frontend) and widgets.", # Crucial for delegation
         instruction=PORTAL_AGENT_PROMPT,
-        tools=[tools.get_widgets, tools.check_yamls, tools.apply_manifest]
-        # tools=[tools.get_widgets, tools.check_yamls, tools.apply_manifest, restaction_agent_tool]
+        tools=[tools_portal_agent.get_widgets, tools_portal_agent.create_file, tools_portal_agent.apply_manifest],
     )
     print(f"✅ Agent '{portal_agent.name}' created using model '{portal_agent.model}'.")
 except Exception as e:
     print(f"❌ Could not create '{portal_agent.name}' agent. Check API Key ({portal_agent.model}). Error: {e}")    
-# portal_agent_tool = agent_tool.AgentTool(agent=portal_agent) # Wrap the agent
 
 # --- Composition Agent ---
 composition_agent = None
@@ -84,8 +82,9 @@ try:
         model=GEMINI_2_5_PRO,
         name="composition_agent",
         instruction=COMPOSITION_AGENT_PROMPT,
-        description="Creates Krateo compositions.", # Crucial for delegation
-        tools=[tools.create_file, tools.apply_manifest, tools.gen_values_schema_json, github_agent_tool]
+        description="Creates Krateo compositions."# Crucial for delegation
+                    "Can apply manifests (e.g. composition, compositiondefinition) to the cluster.",
+        tools=[tools.create_file, tools.apply_manifest, tools.gen_values_schema_json]
     )
     print(f"✅ Agent '{composition_agent.name}' created using model '{composition_agent.model}'.")
 except Exception as e:
@@ -98,7 +97,7 @@ try:
         model=GEMINI_2_5_FLASH,
         name="auth_agent",
         instruction=AUTHENTICATION_AGENT_PROMPT,
-        description="Handles Krateo login, authentication, or password management" # Crucial for delegation
+        description="Handles Krateo login and authentication" # Crucial for delegation
                     "It creates accounts in Krateo and answers questions about authentication."
                     "Handles Questions about `authn`, user `Secrets`, or the `User` custom resource in Krateo.",
         tools=[tools.apply_manifest, tools.get_admin_psw],
@@ -125,10 +124,8 @@ root_agent = Agent(
     name="krateo_autopilot",
     model=GEMINI_2_5_FLASH,
     description="The main coordinator agent. Handles general questions about Krateo and delegates specific tasks to sub-agents."
-                "It uses the composition_agent to create Krateo compositions and the portal_agent for creating portal sections."
-                "It uses the documentation agent to answer questions about Krateo"
-                "It uses the install_krateo tool to install Krateo PlatformOps on the current Kubernetes cluster.",
+                "It uses the `install_krateo` tool to install Krateo PlatformOps on the current Kubernetes cluster.",
     instruction=ROOT_AGENT_PROMPT,
     tools=[tools.install_krateo],
-    sub_agents=[composition_agent, portal_agent, documentation_agent, auth_agent]
+    sub_agents=[composition_agent, portal_agent, documentation_agent, auth_agent, github_agent]
 )
